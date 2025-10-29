2465f26388516bb0c946b4d4c1075b17d8985928
BarminaSK <barminask@mail.ru>
2025-10-29 08:27:32 +0500

    Task6

---
+++ Tyuiu.BarminaSK.Sprint3.Task6.V20.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.BarminaSK.Sprint3.Task6.V20.Lib;
+
+namespace Tyuiu.BarminaSK.Sprint3.Task6.V20.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int startValue = 20;
+            int stopValue = 32;
+            int res = ds.GetSumTheDivisors(startValue, stopValue);
+            int wait = 41;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BarminaSK.Sprint3.Task6.V20/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.BarminaSK.Sprint3.Task6.V20.Lib;
+
+namespace Tyuiu.BarminaSK.Sprint3.Task6.V20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнила: Бармина С.К. | ПИНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема:  Вложенные циклы                                                  *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #22                                                             *");
+            Console.WriteLine("* Выполнила: Бармина София Константиновна | ПИНб-25-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = 20;
+            int stopValue = 32;
+
+            Console.WriteLine($"Старт диапазона = {startValue}");
+            Console.WriteLine($"Конец диапазона = {stopValue}");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int result = ds.GetSumTheDivisors(startValue, stopValue);
+            Console.WriteLine($"Количество делителей меньше 12 = {result}");
+
+            Console.ReadKey();
+        }
+    }
+}
